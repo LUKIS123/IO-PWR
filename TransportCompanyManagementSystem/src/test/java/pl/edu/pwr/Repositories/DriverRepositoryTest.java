4f859ef83ebef487cef3b06484daf444493d70fc
Lukasz Gawron <51411768+LUKIS123@users.noreply.github.com>
2024-01-10 16:04:47 +0100

    test project template

---
+++ TransportCompanyManagementSystem/src/test/java/pl/edu/pwr/Repositories/DriverRepositoryTest.java
@@ -0,0 +1,28 @@
+package pl.edu.pwr.Repositories;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DriverRepositoryTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getAvailableDrivers() {
+    }
+}
