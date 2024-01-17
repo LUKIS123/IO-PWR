5b4b233abfbc79b2dafe4cdf8fd24575c011daa4
Dom <szborzdynski@gmail.com>
2024-01-17 13:59:54 +0100

    Exception handler

---
+++ TransportCompanyManagementSystem/src/main/java/pl/edu/pwr/application/ExceptionHandlerExtension.java
@@ -0,0 +1,12 @@
+package pl.edu.pwr.application;
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.TestExecutionExceptionHandler;
+
+public class ExceptionHandlerExtension implements TestExecutionExceptionHandler {
+    @Override
+    public void handleTestExecutionException(ExtensionContext context, Throwable throwable) throws Throwable {
+        System.out.println("Błąd testu: " + throwable.getMessage()); // w konsoli nie wypisze ale moze tu coś być
+        throw throwable; // jeszcze raz throw żeby test failował
+    }
+}
